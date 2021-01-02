1c2ef93bea1cb18356e929f0d5471272e55db7c8
John Hughes <hughesj17@gmail.com>
2021-01-01 16:32:17 -0800

    Initial build submit
    Added git submodules and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// main.cpp
+//------------------------------------------------------------------------------
+// Copyright (c) 2021 John Hughes
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+// Headers
+//------------------------------------------------------------------------------
+#include "ae/aeClock.h"
+#include "ae/aeInput.h"
+#include "ae/aeLog.h"
+#include "ae/aeRender.h"
+#include "ae/aeWindow.h"
+
+//------------------------------------------------------------------------------
+// Main
+//------------------------------------------------------------------------------
+int main()
+{
+	AE_LOG( "Initialize" );
+
+	aeWindow window;
+	aeRender render;
+	aeInput input;
+
+	window.Initialize( 800, 600, false, true );
+	window.SetTitle( "Game" );
+	render.InitializeOpenGL( &window );
+	input.Initialize( &window );
+
+	aeFixedTimeStep timeStep;
+	timeStep.SetTimeStep( 1.0f / 60.0f );
+
+	while ( !input.GetState()->exit )
+	{
+		input.Pump();
+		render.Activate();
+		render.Clear( aeColor::PicoDarkPurple() );
+		render.Present();
+		timeStep.Wait();
+	}
+
+	AE_LOG( "Terminate" );
+
+	input.Terminate();
+	render.Terminate();
+	window.Terminate();
+
+	return 0;
+}
